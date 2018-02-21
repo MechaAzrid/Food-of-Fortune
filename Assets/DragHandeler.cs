986b026615d208814113b4fd45c8aebc8ac237b7
davidmarywhite <1009198@student.sae.edu.au>
2018-02-22 10:41:26 +1100

    Drag and Drop on the way

---
+++ Assets/DragHandeler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Transform startParent;
+
+   
+    
+    #region IBeginDragHandler implementation
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+    #endregion
+
+
+
+
+
+    #region IDragHandler implementation
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        transform.position = Input.mousePosition;
+
+    }
+    #endregion
+
+
+
+
+
+    #region IDragHandler implementation
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        itemBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if(transform.parent != startParent)
+        {
+            transform.position = startPosition;
+        }
+
+    }
+    #endregion
+
+}
