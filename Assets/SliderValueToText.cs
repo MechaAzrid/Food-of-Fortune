f013ae7a47d4ae1857dd1487e6b442f41888630b
winstonwong9 <winstonwong9@gmail.com>
2018-02-22 22:17:26 +1100

    slider text and slider text values
    the slider will now show the values when  adjusting the handle 1-10

---
+++ Assets/SliderValueToText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class SliderValueToText : MonoBehaviour {
+
+    Text textComponent;
+
+    // Use this for initialization
+    void Start () {
+        textComponent = GetComponent<Text>();
+		
+	}
+
+    public void SetSliderValue(float sliderValue)
+    {
+        textComponent.text = Mathf.Round(sliderValue *10).ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
