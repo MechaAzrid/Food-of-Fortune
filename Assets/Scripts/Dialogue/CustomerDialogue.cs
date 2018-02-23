208e97506d7195d49d1f8d6d2af237a5afa4295e
milzinberry <a.prevedoros@gmail.com>
2018-02-23 11:13:05 +1100

    Dialogue to Textbox

--- Assets/Scripts/Dialogue/CustomerDialogue.cs
+++ Assets/Scripts/Dialogue/CustomerDialogue.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomerDialogue : MonoBehaviour
 {
     public int customerFoodValue = 0;
+    public Text responseText;
 
     void Start()
     {
         string responseToDisplay = gameObject.GetComponent<Health.HealthResponses>().GetHealthResponse(customerFoodValue);
 
-        print(responseToDisplay);
+        responseText.text = responseToDisplay;
+
+        //print(responseToDisplay);
     }
 }
