91efb637cd4e9ebe077d556171f1b8e981b966b0
Mark <32753431+MechaAzrid@users.noreply.github.com>
2018-02-23 11:31:10 +1100

    Added in customer name next to dialogue prompt
    Name has been added (commented out) so it appears next to the text when prompted, placed old dialogue texts in a folder

--- Assets/Scripts/Dialogue/CustomerDialogue.cs
+++ Assets/Scripts/Dialogue/CustomerDialogue.cs
@@ -5,8 +5,14 @@ using UnityEngine.UI;
 
 public class CustomerDialogue : MonoBehaviour
 {
+    [Header("Customer Food Value")]
     public int customerFoodValue = 0;
+
+    [Header("Text Box UI")]
     public Text responseText;
+
+    [Header("Customer Name - If Applicable")]
+    public string customerName;
 
     void Start()
     {
@@ -14,6 +20,7 @@ public class CustomerDialogue : MonoBehaviour
 
         responseText.text = responseToDisplay;
 
-        //print(responseToDisplay);
+        //responseText.text = (customerName + " : " + responseToDisplay);
+            //Used if we want customers to have a name and have it show up when the text appears
     }
 }
