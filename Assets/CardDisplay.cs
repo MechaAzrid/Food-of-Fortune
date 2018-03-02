a945291ed30f25a5a98b32c4e06a676506182285
winstonwong9 <winstonwong9@gmail.com>
2018-03-02 11:13:49 +1100

    inventory cards working :)

---
+++ Assets/CardDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CardDisplay : MonoBehaviour
+{
+
+    public Card card;
+
+    public Text nameText;
+    public Text descriptionText;
+
+    public Image artworkImage;
+
+    public Text saltText;
+    public Text quantityText;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+
+        artworkImage.sprite = card.artwork;
+
+        saltText.text = card.salt.ToString();
+        quantityText.text = card.quantity.ToString();
+    }
+}
