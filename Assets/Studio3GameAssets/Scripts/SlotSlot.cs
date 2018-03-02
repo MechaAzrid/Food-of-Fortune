b20aee6ea879bef90be585b5bc0a9c59051de2ef
winstonwong9 <winstonwong9@gmail.com>
2018-03-02 14:54:17 +1100

    Winston Pusher

--- Assets/Studio3GameAssets/Scripts/SlotSlot.cs
+++ Assets/Studio3GameAssets/Scripts/SlotSlot.cs
@@ -14,13 +14,7 @@ public class SlotSlot : MonoBehaviour, IDropHandler {
                 return transform.GetChild (0).gameObject;
             }
             return null;
-
-
-            
         }
-
-
-        
 
 
 		}
@@ -31,12 +25,11 @@ public class SlotSlot : MonoBehaviour, IDropHandler {
     #region IDropHandler implementation
     public void OnDrop(PointerEventData eventData)
     {
-        if (!item && gameObject.tag == "Cooking")
+        if (!item)
         {
             DragHandeler.itemBeingDragged.transform.SetParent(transform);
             ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
         }
-        
     }
     #endregion
 }
