c109d7b6277a9934dbdeb71776f33f56126c0e35
davidmarywhite <1009198@student.sae.edu.au>
2018-03-09 11:06:43 +1100

    fruitmangoscript

---
+++ Assets/FruitSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public GameObject Object;
+    public Transform spawnPosition;
+
+
+    GameObject cloneObject;
+
+    public void SpawnMango()
+    {
+
+        Instantiate(Object, this.transform.position, this.transform.rotation, this.transform); 
+
+
+        Object.transform.parent = this.transform; 
+    }
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
