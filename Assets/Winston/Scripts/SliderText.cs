60d301886ee656cc95a2384fe3e2b7f625fc7bff
winstonwong9 <winstonwong9@gmail.com>
2018-03-11 23:24:44 +1100

    ////

---
+++ Assets/Winston/Scripts/SliderText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour {
+
+	Text textComponent;
+
+	void Start() {
+		textComponent = GetComponent<Text>();
+	}
+
+	public void SetSliderValue(float sliderValue) {
+		textComponent.text = Mathf.Round(sliderValue * 100).ToString();
+	}
+}
