c7e07fe6b8559dd043c2f9c55a6730735603d80e
d-wade-sae <damon1w@optusnet.com.au>
2018-03-12 14:48:56 +1100

    Debug Menu Changes
    Attempting to make the buttons bigger

--- Assets/Master Asset Folder/Scripts/DebugMenu.cs
+++ Assets/Master Asset Folder/Scripts/DebugMenu.cs
@@ -11,34 +11,39 @@ public class DebugMenu : MonoBehaviour {
         CM = GameObject.Find("_CustomerManager").GetComponent<CustomerManager>();
     }
 
+    void Start()
+    {
+        
+    }
+
     void OnGUI()
     {
-        GUI.Label(new Rect(2820, 60, 100, 30), "Debug Menu");
+        GUI.Label(new Rect(130, 60, 100, 30), "Debug Menu");
 
-        if (GUI.Button(new Rect(2820, 100, 120, 30), "Increase Gold 10"))
+        if (GUI.Button(new Rect(100, 100, 120, 30), "Increase Gold 10"))
         {
             GameManager.instance.playerGold += 10;
         }
-        if (GUI.Button(new Rect(2820, 140, 120, 30), "Decrease Gold 10"))
+        if (GUI.Button(new Rect(100, 140, 120, 30), "Decrease Gold 10"))
         {
             GameManager.instance.playerGold -= 10;
         }
-        if (GUI.Button(new Rect(2820, 180, 120, 30), "Spawn Customer"))
+        if (GUI.Button(new Rect(100, 180, 120, 30), "Spawn Customer"))
         {
             CM.Invoke("SpawnCustomerButton", 0);
         }
 
-        if (GUI.Button(new Rect(2820, 220, 120, 30), "Correct Order"))
+        if (GUI.Button(new Rect(100, 220, 120, 30), "Correct Order"))
         {
             CM.Invoke("CompleteOrderCorrectlyButton", 0);
         }
 
-        if (GUI.Button(new Rect(2820, 260, 120, 30), "Incorrect Order"))
+        if (GUI.Button(new Rect(100, 260, 120, 30), "Incorrect Order"))
         {
             CM.Invoke("CompleteOrderIncorrectlyButton", 0);
         }
 
-        if (GUI.Button(new Rect(2820, 300, 120, 30), "Run Average"))
+        if (GUI.Button(new Rect(100, 300, 120, 30), "Run Average"))
         {
             GameManager.instance.Invoke("UpdateHealthMeter", 0);
         }
