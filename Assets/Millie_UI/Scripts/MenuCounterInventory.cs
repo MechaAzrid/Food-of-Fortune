e9b29f1526fd24a8d0c723b3a76ba454f037bab9
Millie <a.prevedoros@gmail.com>
2018-03-14 01:30:44 +1100

    New Menu Selection Scene
    New Menu Item Selection

---
+++ Assets/Millie_UI/Scripts/MenuCounterInventory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuCounterInventory : MonoBehaviour {
+
+    public float counter = 0;
+    public GameObject Continue;
+    public GameObject chooseAgain;
+
+    //public GameObject UIButtons;
+
+    //public MenuSelectionScript MS;
+
+     
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if two menu buttons are selected then show continue button and disable others
+
+        if (counter >= 2)
+        {
+
+            
+            Continue.SetActive(true);
+            chooseAgain.SetActive(true);
+
+            
+            //show button and disable other buttons
+        }
+
+        else
+        {
+            
+            Continue.SetActive(false);
+            chooseAgain.SetActive(false);
+        }
+    }
+
+
+
+    //public void ContinueButton()
+    //{
+    //save menu choices and load new scene
+    //}
+
+
+    //public void ChooseAgainButton()
+    //{
+
+    //    UIButtons.GetComponent<Button>().interactable = true;
+
+    //}
+
+
+
+
+}
---
+++ Assets/Millie_UI/Scripts/MenuSelectionScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSelectionScript : MonoBehaviour {
+
+
+    public GameObject correspondingIngredients;
+    public Button menuItems;
+    public Color selectedItem;
+    public Color unselectedItem;
+    public Image colour;
+
+    public MenuCounterInventory MC;
+
+    public bool clicked = false;
+
+  
+    //public Color disabled;
+   
+
+    
+    // Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (MC.counter >= 2)
+        {
+            menuItems.GetComponent<Button>().interactable = false;
+        }
+
+        else
+        {
+            menuItems.GetComponent<Button>().interactable = true;
+        }
+        
+
+
+    }
+
+    //void "MenuItems" Buttons here - make corresponding ingredients visible - save quantity
+
+    public void MenuItemButton()
+    {
+
+        if (clicked == false)
+        {
+            correspondingIngredients.SetActive(true);
+            colour.GetComponent<Image>().color = selectedItem;
+            clicked = true;
+            MC.counter = MC.counter + 1;
+        }
+
+        else
+        {
+            clicked = false;
+            correspondingIngredients.SetActive(false);
+            colour.GetComponent<Image>().color = unselectedItem;
+
+            MC.counter = MC.counter - 1;
+
+        }
+    }
+
+    //public void ChooseAgainButton()
+    //{
+
+    //    menuItems.GetComponent<Button>().interactable = true;
+
+    //}
+
+
+
+
+
+}
