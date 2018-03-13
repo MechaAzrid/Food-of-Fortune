0fffa404df38953031d8d7493204b7a48e141184
davidmarywhite <1009198@student.sae.edu.au>
2018-03-13 21:43:22 +1100

    ConvertedToNumbers

---
+++ Assets/NumberChange.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberChange : MonoBehaviour{
+
+    public GameObject UIButtonPlus;
+    public GameObject UIButtonMinus;
+
+
+    
+    public Text CenterText;
+
+    public int numberText;
+    
+    void Start()
+    {
+       
+    }
+
+    public void ClickPlus()
+    {
+        //numberText.ToString();
+        CenterText.text = numberText.ToString() ;
+        numberText += 1;
+
+    }
+
+    public void ClickMinus()
+    {
+        CenterText.text = numberText.ToString();
+        numberText -= 1;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+
+
+}
