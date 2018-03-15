eaeedb32529077674ecd31ace154c35db3f3a4a1
davidmarywhite <1009198@student.sae.edu.au>
2018-03-15 14:15:46 +1100

    Fruit shop

--- Assets/UIManagerScript.cs
+++ Assets/UIManagerScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManagerScript : MonoBehaviour {
-
+    //prefabs
     public GameObject MangoButton;
     public GameObject OnionButton;
     public GameObject LettuceButton;
@@ -15,59 +15,75 @@ public class UIManagerScript : MonoBehaviour {
     public GameObject CheeseButton;
     public GameObject SausageButton;
 
-	// Use this for initialization
-	void Start () {
+    //Hierachy button position
+    public GameObject MangoButtonHierarchy;
+    public GameObject OnionButtonHierarchy;
+    public GameObject LettuceButtonHierarchy;
+    public GameObject PotatoButtonHierarchy;
+    public GameObject HamburgerButtonHierarchy;
+    public GameObject AppleButtonHierarchy;
+    public GameObject ShallotButtonHierarchy;
+    public GameObject CarrotButtonHierarchy;
+    public GameObject CheeseButtonHierarchy;
+    public GameObject SausageButtonHierarchy;
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 
     public void ButtonMango()
     {
-        Instantiate(MangoButton, MangoButton.transform.position, MangoButton.transform.rotation, MangoButton.transform.parent);
+        Instantiate(MangoButton, MangoButton.transform.position, MangoButton.transform.rotation, MangoButtonHierarchy.transform.parent);
     }   
 
     public void ButtonOnion()
     {
-        Instantiate(OnionButton, OnionButton.transform.position, OnionButton.transform.rotation, OnionButton.transform.parent);
+        Instantiate(OnionButton, OnionButton.transform.position, OnionButton.transform.rotation, OnionButtonHierarchy.transform.parent);
 
     }
     public void ButtonLettuce()
     {
 
-        Instantiate(LettuceButton, LettuceButton.transform.position, LettuceButton.transform.rotation, LettuceButton.transform.parent);
+        Instantiate(LettuceButton, LettuceButtonHierarchy.transform.position, LettuceButtonHierarchy.transform.rotation, LettuceButtonHierarchy.transform.parent);
     }
     public void ButtonPotato()
     {
-        Instantiate(PotatoButton, PotatoButton.transform.position, PotatoButton.transform.rotation, PotatoButton.transform.parent);
+        Instantiate(PotatoButton, PotatoButtonHierarchy.transform.position, PotatoButtonHierarchy.transform.rotation, PotatoButtonHierarchy.transform.parent);
 
     }
     public void ButtonHamburger()
     {
-        Instantiate(HamburgerButton, HamburgerButton.transform.position, HamburgerButton.transform.rotation, HamburgerButton.transform.parent);
+        Instantiate(HamburgerButton, HamburgerButtonHierarchy.transform.position, HamburgerButtonHierarchy.transform.rotation, HamburgerButtonHierarchy.transform.parent);
 
     }
     public void ButtonApple()
     {
 
-        Instantiate(AppleButton, AppleButton.transform.position, AppleButton.transform.rotation, AppleButton.transform.parent);
+        Instantiate(AppleButton, AppleButtonHierarchy.transform.position, AppleButtonHierarchy.transform.rotation, AppleButtonHierarchy.transform.parent);
     }
     public void ButtonShallot()
     {
-        Instantiate(ShallotButton, ShallotButton.transform.position, ShallotButton.transform.rotation, ShallotButton.transform.parent);
+        Instantiate(ShallotButton, ShallotButtonHierarchy.transform.position, ShallotButtonHierarchy.transform.rotation, ShallotButtonHierarchy.transform.parent);
 
     }
     public void ButtonCarrot()
     {
-        Instantiate(CarrotButton, CarrotButton.transform.position, CarrotButton.transform.rotation, CarrotButton.transform.parent);
+        Instantiate(CarrotButton, CarrotButtonHierarchy.transform.position, CarrotButtonHierarchy.transform.rotation, CarrotButtonHierarchy.transform.parent);
 
     }
     public void ButtonCheese()
     {
-        Instantiate(CheeseButton, CheeseButton.transform.position, CheeseButton.transform.rotation, CheeseButton.transform.parent);
+        Instantiate(CheeseButton, CheeseButtonHierarchy.transform.position, CheeseButtonHierarchy.transform.rotation, CheeseButtonHierarchy.transform.parent);
 
     }
     public void ButtonSausage()
     {
-        Instantiate(SausageButton, SausageButton.transform.position, SausageButton.transform.rotation, SausageButton.transform.parent);
+        Instantiate(SausageButton, SausageButtonHierarchy.transform.position, SausageButtonHierarchy.transform.rotation, SausageButtonHierarchy.transform.parent);
 
     }
 
