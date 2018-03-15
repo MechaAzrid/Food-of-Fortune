bdb7c22f837eb5ebeb412441253e470853a5f15f
davidmarywhite <1009198@student.sae.edu.au>
2018-03-15 13:43:23 +1100

    HasChanged deleted

--- Assets/SlotSlot2.cs
+++ Assets/SlotSlot2.cs
@@ -1,71 +1,71 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿//using System.Collections;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
 
-public class SlotSlot2 : MonoBehaviour,IDropHandler {
+//public class SlotSlot2 : MonoBehaviour,IDropHandler {
  
    
 
-    public GameObject item2
-    {
+//    public GameObject item2
+//    {
 
-        get
-        {
-            if (transform.childCount > 0)
-            {
-                return transform.GetChild(0).gameObject;
-            }
-            return null;
-        }
-
-
-    }
+//        get
+//        {
+//            if (transform.childCount > 0)
+//            {
+//                return transform.GetChild(0).gameObject;
+//            }
+//            return null;
+//        }
 
 
-    //public string itemString
-    //{
-
-    //    get
-    //    {
-    //        if (transform.childCount > 0)
-    //        {
-    //            return transform.GetChild(0).gameObject.tag;
-    //        }
-    //        return null;
-    //    }
+//    }
 
 
+//    //public string itemString
+//    //{
+
+//    //    get
+//    //    {
+//    //        if (transform.childCount > 0)
+//    //        {
+//    //            return transform.GetChild(0).gameObject.tag;
+//    //        }
+//    //        return null;
+//    //    }
 
 
-    //}
 
 
-    #region IDropHandler implementation
-    public void OnDrop(PointerEventData eventData)
-    {
-        if (!item2)
-        {
+//    //}
 
-            DragHandeler.itemBeingDragged.transform.SetParent(transform);
-            ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
 
-        }
-        else
-        {
+//    #region IDropHandler implementation
+//    public void OnDrop(PointerEventData eventData)
+//    {
+//        if (!item2)
+//        {
+
+//            DragHandeler.itemBeingDragged.transform.SetParent(transform);
+//            ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
+
+//        }
+//        else
+//        {
             
 
-        }
-    }
-    #endregion
+//        }
+//    }
+//    #endregion
 
 
-    void Update()
-    {
+//    void Update()
+//    {
 
-     //   Debug.Log(itemString);
-       // Debug.Log(item);
+//     //   Debug.Log(itemString);
+//       // Debug.Log(item);
       
-    }
+//    }
 
 
-}
+//}
