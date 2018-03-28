5c6708590c9b17e3eb94282041c61e8a6cb4a521
davidmarywhite <1009198@student.sae.edu.au>
2018-03-29 05:11:51 +1100

    All snap/animation completed for preparation

--- Assets/Studio3GameAssets/Scripts/SlotSlot.cs
+++ Assets/Studio3GameAssets/Scripts/SlotSlot.cs
@@ -38,38 +38,74 @@ public class SlotSlot : MonoBehaviour, IDropHandler {
             itembeingdruggedslot = item;
         }
 
+        //Preparing Mango Code
+        if (itembeingdruggedslot.tag.Contains("Mango") && this.gameObject.tag == "ChoppingBoard")
+        {
+            //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
+            itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
+            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
+           
+        }
+
+        //Preparing Lettuce Code
+        if (itembeingdruggedslot.tag.Contains("Lettuce") && this.gameObject.tag == "ChoppingBoard")
+        {
+            //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
+            itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
+            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
+          
+        }
+
+        //Preparing Lettuce Code 
+        if (itembeingdruggedslot.tag.Contains("Onion") && this.gameObject.tag == "ChoppingBoard")
+        {
+            //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
+            itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
+            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
+            
+        }
+
+        //Preparing Carrot Code 
+        if (itembeingdruggedslot.tag.Contains("Carrot") && this.gameObject.tag == "ChoppingBoard")
+        {
+            //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
+            itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
+            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
+           
+        }
+
+        //Preparing Potato Code 
+        if (itembeingdruggedslot.tag.Contains("Potato") && this.gameObject.tag == "ChoppingBoard")
+        {
+            //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
+            itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
+            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
+            itembeingdruggedslot.transform.GetChild(2).gameObject.SetActive(false);
+
+        }
+
+
         //Preparing Apple Code
         if (itembeingdruggedslot.tag.Contains("Apple")&& this.gameObject.tag == "ChoppingBoard")
         {
             //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
-            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
             itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
+            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
+          
         }
 
         //Preparing Patty Code
         if (itembeingdruggedslot.tag.Contains("Patty") && this.gameObject.tag == "Fryer")
         {
             //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
-            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
             itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
-        }
-
-        //Preparing Mango Code
-        if (itembeingdruggedslot.tag.Contains("Mango") && this.gameObject.tag == "ChoppingBoard")
-        {
-            //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
             itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
-            itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
+           
         }
 
 
-        //Preparing Mango Code
-        if (itembeingdruggedslot.tag.Contains("Mango") && this.gameObject.tag == "ChoppingBoard")
-        {
-            //itembeingdruggedslot.GetComponentInChildren<GameObject>().SetActive(false);
-            itembeingdruggedslot.transform.GetChild(1).gameObject.SetActive(true);
-            itembeingdruggedslot.transform.GetChild(0).gameObject.SetActive(false);
-        }
+
+   
 
 
     }
