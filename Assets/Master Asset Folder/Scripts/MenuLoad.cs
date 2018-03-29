f9c49821697a355a16a1724a9ea47fafb599754c
Mark <32753431+MechaAzrid@users.noreply.github.com>
2018-03-29 11:16:27 +1100

    Updated the main menu
    Working on the scene changes

---
+++ Assets/Master Asset Folder/Scripts/MenuLoad.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoad : MonoBehaviour
+{
+    [Header("Canvas Groups")]
+    public Transform mainCanvas;
+    public Transform instructionsCanvas;
+
+    [Header("Particle Systems")]
+    public ParticleSystem particleSys;
+    public ParticleSystem particleSys2;
+    public ParticleSystem particleSys3;
+
+    public void LoadMasterScene(string scene)
+    {
+        SceneManager.LoadScene("Master_Scene");
+    }
+
+    public void LoadInstructionsScene()
+    {
+        if (instructionsCanvas.gameObject.activeInHierarchy == false)
+            //if the Instructions Canvas is inactive then when we press "Instructions" enable the canvas but disable the mainCanvas
+        {
+            instructionsCanvas.gameObject.SetActive(true);
+            mainCanvas.gameObject.SetActive(false);
+            particleSys.gameObject.SetActive(false);
+            particleSys2.gameObject.SetActive(false);
+            particleSys3.gameObject.SetActive(false);
+
+        }
+
+        else
+        {
+            instructionsCanvas.gameObject.SetActive(false);
+            particleSys.gameObject.SetActive(true);
+            particleSys2.gameObject.SetActive(true);
+            particleSys3.gameObject.SetActive(true);
+        }
+    }
+
+    public void ReturnToMainScene()
+    {
+        if (mainCanvas.gameObject.activeInHierarchy == false)
+        {
+            mainCanvas.gameObject.SetActive(true);
+            instructionsCanvas.gameObject.SetActive(false);
+            particleSys.gameObject.SetActive(true);
+            particleSys2.gameObject.SetActive(true);
+            particleSys3.gameObject.SetActive(true);
+
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
