1b5b29fcca8137aea6ed1e939ccdf5a8d7e515e4
d-wade-sae <1007115@student.sae.edu.au>
2018-04-12 10:09:49 +1000

    End of Shift Changes

--- Assets/Master Asset Folder/Scripts/EndOfShift.cs
+++ Assets/Master Asset Folder/Scripts/EndOfShift.cs
@@ -34,7 +34,6 @@ public class EndOfShift : MonoBehaviour {
         save.SetActive(false);
 
         StartCoroutine(LoadResults());
-        GameManager.instance.SaveGame();
     }
 
     public IEnumerator LoadResults()
